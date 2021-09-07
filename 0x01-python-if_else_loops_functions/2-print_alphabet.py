b07b7491ebc41bd5ade673fc85a86549b718567e
sebagadisk <sebexk3@gmail.com>
2021-09-07 19:36:31 +0000

    modified 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for a in range(ord('a'), ord('z') + 1):
-    print("{:c}".format(a), end='')
+for a in range(ord("a"), ord("z") + 1):
+    if chr(a) is not "q" and chr(a) is not "e":
+        print("{:s}".format(chr(a)), end="")
