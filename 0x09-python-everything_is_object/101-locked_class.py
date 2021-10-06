9b9ece58ac917799cddaab4f130077ae941e9844
SebagadisK <sebexk3@gmail.com>
2021-10-06 10:24:10 -0700

    Modified 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-class LockedClass:
-    __slots__ = ['first_name']
+""" LockedClass class """
+
+
+class LockedClass():
+    """ variables declarations """
+    __slots__ = ('first_name')
