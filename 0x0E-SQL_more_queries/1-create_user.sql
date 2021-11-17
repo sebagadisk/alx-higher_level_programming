33acac2280e84f324572ca594186961c007fe0e5
SebagadisK <sebexk3@gmail.com>
2021-11-17 15:51:31 -0800

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
-- creates the MySQL server user user_0d_1.
+-- creates the MySQL server user user_0d_1.
 -- user_0d_1 should have all privileges on your MySQL server
 -- The user_0d_1 password should be set to user_0d_1_pwd
 -- If the user user_0d_1 already exists, your script should not fail
